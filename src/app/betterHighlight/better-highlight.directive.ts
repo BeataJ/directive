9318ddb4ff973c024618bc5edebcbaf5ce9fa83a
beataj <beata@emc2wave.com>
2021-07-05 16:19:29 -0700

    add @HostBinding

--- src/app/betterHighlight/better-highlight.directive.ts
+++ src/app/betterHighlight/better-highlight.directive.ts
@@ -1,9 +1,17 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { 
+  Directive, 
+  ElementRef, 
+  HostBinding, 
+  HostListener, 
+  OnInit, 
+  Renderer2
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
