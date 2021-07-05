418ff65c7f13c077a3292a3a581de0e42e0356e1
beataj <beata@emc2wave.com>
2021-07-04 18:33:16 -0700

    add ElementRef

--- src/app/betterHighlight/better-highlight.directive.ts
+++ src/app/betterHighlight/better-highlight.directive.ts
@@ -1,14 +1,14 @@
-import { Directive, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer
+    this.renderer.setStyle()
   }
 
 }
