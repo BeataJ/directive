d3c978e8760f0802c85be7bbba9030350cc3945d
beataj <beata@emc2wave.com>
2021-07-04 18:20:07 -0700

    add onInit

--- src/app/betterHighlight/better-highlight.directive.ts
+++ src/app/betterHighlight/better-highlight.directive.ts
@@ -1,10 +1,14 @@
-import { Directive, Renderer2 } from '@angular/core';
+import { Directive, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
 
   constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.renderer
+  }
 
 }
